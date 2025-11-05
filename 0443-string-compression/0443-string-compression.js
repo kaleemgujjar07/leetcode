8e786228a0962dc2417d3e2e237f8eadf984276d
Kaleem gujjar <160313116+kaleemgujjar07@users.noreply.github.com>
2025-11-05 20:37:37 +0500

    Time: 4 ms (28.13%), Space: 58.7 MB (14.55%) - LeetHub

---
+++ 0443-string-compression/0443-string-compression.js
@@ -0,0 +1,29 @@
+/**
+ * @param {character[]} chars
+ * @return {number}
+ */
+var compress = function(chars) {
+    let read = 0;
+    let write = 0;
+
+    while(read < chars.length){
+        let currentChar = chars[read]
+        let count = 0;
+
+        while( read < chars.length && chars[read] === currentChar){
+            read++;
+            count++;
+        }
+
+        chars[write] = currentChar;
+        write++;
+
+        if(count >1){
+            for(let digit of String(count)){
+                chars[write] = digit;
+                write++;
+            }
+        }
+    }
+    return write;
+};
