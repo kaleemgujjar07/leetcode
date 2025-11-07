ef22e619e8a31c70c4b5336291802aaa4069ad26
Kaleem gujjar <160313116+kaleemgujjar07@users.noreply.github.com>
2025-11-07 22:48:57 +0500

    Time: 0 ms (100%), Space: 59 MB (85.42%) - LeetHub

--- 0283-move-zeroes/0283-move-zeroes.js
+++ 0283-move-zeroes/0283-move-zeroes.js
@@ -5,15 +5,15 @@
 var moveZeroes = function(nums) {
     let write = 0;
 
-    
+    //[0,1,0,3,12]
     for (let i = 0; i < nums.length; i++) {
-        if (nums[i] !== 0) {
+        if (nums[i] !== 0) {// 1
             nums[write] = nums[i];
             write++;
         }
     }
 
-    
+    //[1,3,12,3,12]
     while (write < nums.length) {
         nums[write] = 0;
         write++;
