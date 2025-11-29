8001eb947cdfd5c8006bfb8946a8120756ba0d7a
Kaleem gujjar <160313116+kaleemgujjar07@users.noreply.github.com>
2025-11-29 19:26:07 +0500

    Time: 20 ms (85.36%), Space: 70 MB (75.83%) - LeetHub

---
+++ 0219-contains-duplicate-ii/0219-contains-duplicate-ii.js
@@ -0,0 +1,18 @@
+/**
+ * @param {number[]} nums
+ * @param {number} k
+ * @return {boolean}
+ */
+var containsNearbyDuplicate = function(nums, k) {
+    const seen = new Set();
+    for (let i = 0; i < nums.length; i++) {
+        if (seen.has(nums[i])) {
+            return true
+        }
+        seen.add(nums[i])
+        if (seen.size > k) {
+            seen.delete(nums[i-k])
+        }
+    }
+    return false
+};
