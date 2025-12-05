7104b8b8aa1e2df6d59c0689f5ed24c9e132336f
Kaleem gujjar <160313116+kaleemgujjar07@users.noreply.github.com>
2025-12-05 21:44:11 +0500

    Time: 5 ms (64.78%), Space: 57.3 MB (70.93%) - LeetHub

---
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.js
@@ -0,0 +1,25 @@
+/**
+ * @param {string[]} tokens
+ * @return {number}
+ */
+var evalRPN = function (tokens) {
+    let stack = [];
+    for (let c of tokens) {
+        if (c === "+") {
+            stack.push(stack.pop() + stack.pop())
+        } else if (c === "-") {
+            let a = stack.pop()
+            let b = stack.pop()
+            stack.push(b - a)
+        } else if (c === "*") {
+            stack.push(stack.pop() * stack.pop())
+        } else if (c === "/") {
+            let a = stack.pop()
+            let b = stack.pop()
+            stack.push(Math.trunc(b / a))
+        } else {
+            stack.push(Number(c))
+        }
+    }
+    return stack[0]
+};
